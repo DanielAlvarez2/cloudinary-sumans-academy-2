9fffe4396c6e643bbb867d3460074eed53572182
Daniel Alvarez <daniel.yllanes@hotmail.com>
2025-07-03 14:00:16 -0400

    source code incomplete

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,22 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [img,setImg] = useState('')
+  const formdata = new FormData()
+  formdata.append('')
+  const handleClick = ()=>{
+    fetch('/single',{
+      method:'POST',
+      body:formdata
+    })
+    .then(res=>console.log(res.msg))
+    .catch(err=>console.log(err))
+  }
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Upload Image</h1>
+      <input  onChange={(e)=>setImg(e.target.files[0])} 
+              type='file' /><br/>
     </>
   )
 }
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  proxy:{
+    '/':'http://localhost:8187'
+  },
   plugins: [react()],
 })
