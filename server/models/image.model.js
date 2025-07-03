2ceccb41c15d0da3e2491ebbbdc2c864520ad187
Daniel Alvarez <daniel.yllanes@hotmail.com>
2025-07-03 02:25:08 -0400

    add timestamps for db entries

--- server/models/image.model.js
+++ server/models/image.model.js
@@ -2,5 +2,5 @@ const mongoose = require('mongoose')
 const ImageSchema = new mongoose.Schema({
     path:{type:String,required:true},
     filename:{type:String,required:true}
-})
+},{timestamps:true})
 module.exports = mongoose.model('Images', ImageSchema)
